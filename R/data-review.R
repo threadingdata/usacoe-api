0f0145016277a422b9485ef95b887934c054bd0a
threadingdata <dborba@threadingdata.com>
2021-06-11 21:54:13 -0700

    Added all reservoir storage plot to data-review.R

--- R/data-review.R
+++ R/data-review.R
@@ -10,3 +10,14 @@ sha <- usacoe_df %>%
 sha %>%
   ggplot(aes(date, value, color = station)) +
   geom_point()
+
+# Plot major reservoirs ----
+usacoe_df %>%
+  filter(str_detect(name, 'Storage'),
+         !str_detect(name, 'notes'),
+         duration == 'd') %>%
+  ggplot(aes(date, value)) +
+  geom_line() +
+  labs(title = 'Army Corps, Bitches!') +
+  theme_minimal() +
+  facet_wrap(~ name + station + duration, scales = 'free_y', ncol = 4)
